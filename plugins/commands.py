4f2d4571f2af2f6565839d69543d07ac46ff5713
HappyKidBGMZ <105188500+HappyKidBGMZ@users.noreply.github.com>
2022-05-18 11:55:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -147,6 +147,9 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
+                    reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Owner", url=f"https://t.me/happykidbgmz")]
+                    ]),
                     )
             except Exception as e:
                 logger.warning(e, exc_info=True)
