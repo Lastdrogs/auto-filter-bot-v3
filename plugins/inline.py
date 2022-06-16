858db3745ca24529d6bfa7db1270b783e5116687
LᴀsᴛDʀᴏɢᴢ <105853088+Lastdrogz@users.noreply.github.com>
2022-06-17 00:10:55 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -60,7 +60,7 @@ async def answer(bot, query):
                 title=file.file_name,
                 file_id=file.file_id,
                 caption=f_caption,
-            reply_markup=InlineKeyboardMarkup(buttons),
+                reply_markup=InlineKeyboardMarkup(buttons),
                 description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
                 reply_markup=reply_markup))
 
