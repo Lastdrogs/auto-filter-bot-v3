a295acce9de2d185b58f0fc8d25b3af1e451dbe6
LᴀsᴛDʀᴏɢᴢ <105853088+Lastdrogz@users.noreply.github.com>
2022-06-17 14:11:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -475,7 +475,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     chat_id=query.from_user.id,
                     file_id=file_id,
                     caption=f_caption,
-                reply_markup=reply_markup))
+                reply_markup=reply_markup)
 
     if results:
         switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
